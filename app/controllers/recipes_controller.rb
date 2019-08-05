28256f5ecafd12364ea2ebddc39a103d9006c647
Ryan <kerneltux0@gmail.com>
2019-08-05 13:33:31 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,5 +12,12 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    # byebug
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  def ingredients_attributes=(attributes)
+    self.ingredients.build(attributes)
+  end
+
+  def ingredients_attributes
+    self.ingredients
+  end
 end
