85439b1d7c1e51a5e896d98f38b9840e4fe7738a
Ryan Pethel <arpethel@gmail.com>
2019-08-11 01:27:19 +0000

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,4 +13,37 @@ class RecipesController < ApplicationController
 
   def create
   end
+
+
+
+  def new
+    @person = Person.new
+    @person.ingredients.build(address_type: 'work')
+    @person.ingredients.build(address_type: 'home')
+  end
+
+  def create
+    person = Person.create(person_params)
+    redirect_to people_path
+  end
+
+  def index
+    @people = Person.all
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(
+    :name,
+    addresses_attributes: [
+      :street_address_1,
+      :street_address_2,
+      :city,
+      :state,
+      :zipcode,
+      :address_type
+      ]
+    )
+  end
 end
